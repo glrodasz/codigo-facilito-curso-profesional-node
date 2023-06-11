0b6c2d9071cdade17ea179b537b56a6ae5a8b59c
Guillermo Rodas <me@guillermorodas.com>
2023-06-11 23:13:46 +0200

    Tests para services

---
+++ twittr/services/tweetService.test.js
@@ -0,0 +1,22 @@
+const tweetsService = require("./tweetsService");
+const tweetsRepository = require("../repositories/tweetsRepository");
+
+jest.mock("../repositories/tweetsRepository", () => ({
+  getTweets: jest.fn(() => ["tweet1", "tweet2"]),
+}));
+
+describe("[ services/tweetService ]", () => {
+  describe("getTweets", () => {
+    it("should return all tweets", async () => {
+      // Arrange
+      const expected = ["tweet1", "tweet2"];
+
+      // Act
+      const result = await tweetsService.getTweets();
+
+      // Assert
+      expect(result).toEqual(expected);
+	  expect(tweetsRepository.getTweets).toHaveBeenCalledTimes(1)
+    });
+  });
+});
