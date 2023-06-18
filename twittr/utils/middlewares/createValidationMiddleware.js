39db9d8168c19e346a6ec1e167aac68cb21d9137
Guillermo Rodas <me@guillermorodas.com>
2023-06-18 23:47:38 +0200

    Rename notFoundMiddleware and fix naming

--- twittr/utils/middlewares/createValidationMiddleware.js
+++ twittr/utils/middlewares/createValidationMiddleware.js
@@ -3,14 +3,18 @@ const validate = require("../validate");
 
 // @param {Object} validationSchema - { [K in "body"|"query"|"params"]: joiSchema }
 function createValidationMiddleware(validationSchema) {
-  const [[ payload, schema ]] = Object.entries(validationSchema);
+  const [[payloadKey, joiSchema]] = Object.entries(validationSchema);
 
-  if (payload !== "body" && payload !== "query" && payload !== "params") {
-    throw new Error("Invalid payload must be body, query or params");
+  if (
+    payloadKey !== "body" &&
+    payloadKey !== "query" &&
+    payloadKey !== "params"
+  ) {
+    throw new Error("Invalid payload key must be body, query or params");
   }
 
   return function validationMiddleware(req, res, next) {
-    const error = validate(req[payload], schema);
+    const error = validate(req[payloadKey], joiSchema);
     error ? next(error) : next();
     // error ? next(boom.badRequest(error)) : next();
   };
