62384025e71fb09d2428be4c36c6fe3412ddda58
Guillermo Rodas <me@guillermorodas.com>
2023-07-15 17:18:44 +0200

    Despliegue en Fly.io

--- twittr/config/index.js
+++ twittr/config/index.js
@@ -4,7 +4,7 @@ dotenv.config();
 const config = {
   dev: process.env.NODE_ENV !== "production",
   port: process.env.PORT || 3000,
-  databaseUrl: process.env.DATABASE_URL,
+  databaseUrl: `${process.env.DATABASE_URL}?ssl={"rejectUnauthorized":true}`,
   corsOrigin: process.env.CORS_ORIGIN || "https://client.twittr.com",
 };
 
